ca7e173f4c39a786b2443f3c58a553828056cb28
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2024-03-17 09:20:03 -0300

    fix: workaround for false-positive windows defender warning

--- src/renderer/utils/addCacheBustingSuffix.js
+++ src/renderer/utils/addCacheBustingSuffix.js
@@ -1,5 +1,5 @@
 const addCacheBustingSuffix = (url) => {
-    const randomNumber = Math.random();
+    const randomNumber = Math.floor(Math.random() * 999) + 1;
 
     const separator = url.includes('?') ? '&' : '?';
 
